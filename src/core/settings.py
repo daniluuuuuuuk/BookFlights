5f0f0ae2cf980a8285608ad7dd78dc4542845b15
daniluuuuuuuk <54038074+daniluuuuuuuk@users.noreply.github.com>
2019-10-14 19:48:39 +0300

    her

--- src/core/settings.py
+++ src/core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from dynaconf import settings as _settings
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '()sj9yd58_5lnfv1dtlo0&$*=t2(v64wuc*h^bx-k-uu3z)g*n'
+SECRET_KEY = _settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = _settings.DEBUG
 
-ALLOWED_HOSTS = ["bookflights.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = _settings.ALLOWED_HOSTS
 
 
 # Application definition
