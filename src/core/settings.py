1deee8c620946451dab1df1f11f69a91c1e6595c
daniluuuuuuuk <54038074+daniluuuuuuuk@users.noreply.github.com>
2019-10-15 21:06:02 +0300

    Refactoring

--- src/core/settings.py
+++ src/core/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # our apps
-    "home",]
+    "main", ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
