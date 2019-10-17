9616c94031dffa7d73ca1d6a21ef7137749af8af
daniluuuuuuuk <54038074+daniluuuuuuuk@users.noreply.github.com>
2019-10-17 15:26:45 +0300

    first database City

--- src/booking/urls.py
+++ src/booking/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from booking import views
 
-urlpatterns = [path("", TemplateView.as_view(template_name="booking/index.html"), name='booking', )]
+urlpatterns = [path("", views.booking, name="booking")]
--- src/booking/views.py
+++ src/booking/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from booking.models import City
 
 
 def booking(request):
-    return render(request, "booking/index.html")
+    return render(request, "booking/index.html", context={
+        "cities": City.objects.all(),
+    })
+
