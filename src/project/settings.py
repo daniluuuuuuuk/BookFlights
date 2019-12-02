f10e6be2597e4337ab60a9a1f09c45e32dfef183
daniluuuuuuuk <artyom.danilyuk.420@mail.ru>
2019-12-02 19:54:07 +0300

    div error fixed

--- src/project/settings.py
+++ src/project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'django_filters',
+    # 'django_filters',
     # our apps
     "apps.main",
     "apps.booking",
